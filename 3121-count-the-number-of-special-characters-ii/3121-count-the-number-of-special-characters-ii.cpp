6d782f761fec45a49779aeafd3b1ce70096caf4b
pandeyutsav59 <103882589+pandeyutsav@users.noreply.github.com>
2026-05-27 15:17:38 +0530

    Time: 67 ms (73.03%), Space: 24.4 MB (68.82%) - LeetHub

--- 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
+++ 3121-count-the-number-of-special-characters-ii/3121-count-the-number-of-special-characters-ii.cpp
@@ -1,15 +1,28 @@
 class Solution {
 public:
     int numberOfSpecialChars(string word) {
-        bitset<27> A[2];
-
-        for (auto& ch : word) {
-            int i = ch & 31;
-            int Case = (ch >> 5) & 1;
-
-            A[Case].set(i, !(Case & A[0][i]));
+        vector<int>mpl(26 , -1);
+        vector<int>mpu(26 , -1);
+        int n = word.size(); 
+        for(int i = 0 ; i < n ; i++){
+            char a = word[i]; 
+            if(islower(a)){
+                mpl[a-'a'] = i ; 
+            }else{
+                if(mpu[a-'A'] != -1){
+                    continue ;
+                }else{
+                    mpu[a-'A'] = i; 
+                }
+            }
         }
-
-        return (A[0] & A[1]).count();
+        int ans = 0 ; 
+        for(int i = 0 ; i < 26 ; i++){
+            if(mpu[i] != -1 && mpl[i] != -1){
+                if(mpl[i] < mpu[i]) ans++; 
+                else continue ; 
+            }
+        }
+        return ans;
     }
 };
